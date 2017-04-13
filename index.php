fdb6975a50446b798ebbb441706452903e9202dd
Sahil Garg <garg.sahil25@gmail.com>
2017-04-14 01:42:02 +0530

    adding bootstrap

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 
+include_once "services/util.php";
 include_once "services/mysql.php";
 include_once "services/handler.php";
 include_once "services/display.php";
@@ -14,24 +15,27 @@ $txns = getTransactions();
 
 date_default_timezone_set('Asia/Kolkata');
 
+$cashBalance = getBalanceByType('cash'); 
+$clientBalance = getBalanceByType('client');
+
 ?>
 
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
 <title>Ledger</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 <link rel="stylesheet" href="css/style.css"> 
 </head>
 <body>
-	<section>
-		<h1>Ledger
-		<span class="summary"><span class="title">Cash</span> <?php echo getBalanceByType('cash'); ?></span>
-		<span class="summary"><span class="title">Profit</span> <?php echo getBalanceByType('factory') * -1; ?></span> 
-		<span class="summary"><span class="title">Capital</span> <?php echo getBalanceByType('capital'); ?></span>
-		<span class="summary"><span class="title">Home</span> <?php echo getBalanceByType('home'); ?></span>
-		<span class="summary"><span class="title">Client</span> <?php echo getBalanceByType('client'); ?></span>
-		<span class="nav-links"><a href="./accounts.php">Accounts</a><span></h1>
-		<hr>
-	</section>
+	<div class="page-header">
+		<h5>Ledger
+	</div>
+	<div class="summary">
+		<div class="label label-success"><span class="title">Cash</span><?php echo getMoneyFormat($cashBalance); ?></div>
+		<div class="label <?php if ($clientBalance >= 0) { echo 'label-success'; } else { echo 'label-danger'; } ?>"><span class="title">Client</span><?php echo getMoneyFormat($clientBalance); ?></div>
+	</div>
 	<section>
 		<div class="main-block form">
 			<h2>Buy Stuff</h2>
@@ -99,5 +103,7 @@ date_default_timezone_set('Asia/Kolkata');
 			</table>
 		</div>
 	</section>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </body>
 </html>
---
+++ services/util.php
@@ -0,0 +1,17 @@
+<?php
+
+
+function getMoneyFormat($money) {
+	$len = strlen($money);
+    $m = '';
+    $money = strrev($money);
+    for($i=0;$i<$len;$i++){
+        if(( $i==3 || ($i>3 && ($i-1)%2==0) )&& $i!=$len){
+            $m .=',';
+        }
+        $m .=$money[$i];
+    }
+    return "&#8377; ".strrev($m);
+}
+
+?>
