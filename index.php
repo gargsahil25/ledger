88813fb70509457e0a6bc352b64cfa3c8e2172ea
Sahil Garg <garg.sahil25@gmail.com>
2017-04-16 17:07:42 +0530

    update delete txn

--- index.php
+++ index.php
@@ -37,7 +37,7 @@ $profitBalance = getBalanceByType('factory');
 <title>Ledger</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-<link rel="stylesheet" href="css/style.css?v7"> 
+<link rel="stylesheet" href="css/style.css?v8"> 
 </head>
 <body>
 	<div class="loader" style="display:none;"></div>
