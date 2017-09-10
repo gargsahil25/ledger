4ae004a30da9f395e11e7330dac899e48ff6fe00
Sahil Garg <garg.sahil25@gmail.com>
2017-09-11 01:17:45 +0530

    logo

--- index.php
+++ index.php
@@ -47,7 +47,9 @@ $cashId = getAccounts('cash')[0]['id'];
 <title>Ledger</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-<link rel="stylesheet" href="css/style.css?v10"> 
+<link rel="shortcut icon" href="images/icon/icon-128.png" type="image/x-icon" />
+<link id="manifest" rel="manifest" href="manifest.json">
+<link rel="stylesheet" href="css/style.css?v11">
 </head>
 <body>
 	<div class="loader" style="display:none;"></div>
